ec2c022131d345bdc101c3ec038b5a3dd0a2eac4
wenhuchen <hustchenwenhu@gmail.com>
2019-07-24 10:02:42 -0700

    update

--- collect_data.sh
+++ collect_data.sh
@@ -1,8 +1,8 @@
 wget --directory-prefix=data/  https://hdsa-dialog.s3-us-west-2.amazonaws.com/delex.json
 
-mkdir checkpoints
-mkdir checkpoints/predictor
-mkdir checkpoints/generator
+mkdir -p checkpoints
+mkdir -p checkpoints/predictor
+mkdir -p checkpoints/generator
 
 wget --directory-prefix=checkpoints/generator/ https://hdsa-dialog.s3-us-west-2.amazonaws.com/BERT_dim128_w_domain
 wget --directory-prefix=checkpoints/predictor/ https://hdsa-dialog.s3-us-west-2.amazonaws.com/save_step_15120.zip 
